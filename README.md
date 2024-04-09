b01b2d615223d7fab51ef0493ecf8ba1bb379fde
Rocky Emtrika <remtrik@mail.ru>
2024-04-09 13:29:55 +0300

    Sync tools and README.md with Surface Duo repo

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 
-# Windows Driver pack for the POCO X3 Pro
+# Windows Driver pack for Surface Duo platforms
 
-This repository contains driver binary files for the POCO X3 Pro.
-All driver binary files form a board support package to be used on POCO X3 Pro devices to provide hardware support for the Windows operating system.
+![Surface Duo Dual Screen Windows](https://user-images.githubusercontent.com/3755345/197420866-d3bb0534-c848-4cc2-a242-04dae48b0f6e.png)
+
+This repository contains driver binary files for Surface Duo 1 and Surface Duo 2.
+All driver binary files form a board support package to be used on Surface Duo 1 and Surface Duo 2 devices to provide hardware support for the Windows operating system.
 
 These driver files are not perfect, typos may exist, feel free to file an issue on GitHub in case you found any.
 
@@ -19,5 +21,17 @@ Please see [LICENSE](LICENSE.md)
 For preserving charset encoding, please checkout with using:
 
 ```
-git clone -c core.autocrlf=false https://github.com/woa-vayu/Vayu-Drivers
+git clone -c core.autocrlf=false https://github.com/WOA-Project/SurfaceDuo-Drivers
 ```
+
+---
+
+_**© 2020-2024 The Duo WOA Authors**_
+
+_Snapdragon is a registered trademark of Qualcomm Incorporated. Microsoft, the Microsoft Corporate Logo, Windows, Surface, Surface Duo, Windows Hello, Continuum, Hyper-V, and DirectX are registered trademarks of Microsoft Corporation in the United States. Android is a registered trademark of Google LLC. Miracast is a registered trademark of the Wi-Fi Alliance. Other binaries may be copyright Qualcomm Incorporated and Microsoft Surface._
+
+_**Limited emergency calling**_
+
+_Running Windows on your Surface Duo is not a replacement for a proper phone operating system and does not have emergency calling capabilities._
+
+_**Hello from Seattle (US), France, Italy.**_
--- tools/pack-vayu.cmd
+++ tools/pack-vayu.cmd
@@ -2,23 +2,24 @@
 rmdir /Q /S ..\..\Vayu-Drivers-Release
 mkdir ..\..\Vayu-Drivers-Release
 
-echo @echo off > ..\OnlineUpdater_huaxing.cmd
-echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu_huaxing.xml >> ..\OnlineUpdater_huaxing.cmd
+echo @echo off > ..\OnlineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> ..\OnlineUpdater.cmd
+echo pushd "%%~dp0" ^&^& cd %%~dp0 >> ..\OnlineUpdater.cmd
+echo .\tools\DriverUpdater\%%PROCESSOR_ARCHITECTURE%%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu.xml >> ..\OnlineUpdater.cmd
+echo pause >> ..\OnlineUpdater.cmd
 
-echo @echo off > ..\OfflineUpdater_huaxing.cmd
-echo set /P DrivePath=Enter Drive letter ^^^(with the semi-column!^^^) of the connected device in mass storage mode ^^^(e.g. D:^^^): >> ..\OfflineUpdater_huaxing.cmd
-echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu_huaxing.xml -p %%DrivePath%% >> ..\OfflineUpdater_huaxing.cmd
-
-echo @echo off > ..\OnlineUpdater_tianma.cmd
-echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu_tianma.xml >> ..\OnlineUpdater_tianma.cmd
-
-echo @echo off > ..\OfflineUpdater_tianma.cmd
-echo set /P DrivePath=Enter Drive letter ^^^(with the semi-column!^^^) of the connected device in mass storage mode ^^^(e.g. D:^^^): >> ..\OfflineUpdater_tianma.cmd
-echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu_tianma.xml -p %%DrivePath%% >> ..\OfflineUpdater_tianma.cmd
-
-copy DriverUpdater.ARM64.exe ..\
-copy DriverUpdater.AMD64.exe ..\
-copy DriverUpdater.X86.exe ..\
+echo @echo off > ..\OfflineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> ..\OfflineUpdater.cmd
+echo for /f %%%%a in ('wmic logicaldisk where "VolumeName='WINVAYU'" get deviceid^^^|find ":"')do set "DrivePath=%%%%a" >> ..\OfflineUpdater.cmd
+echo if not [%%DrivePath%%]==[] goto start >> ..\OfflineUpdater.cmd
+echo if [%%DrivePath%%]==[] echo Automatic WINVAYU detection failed! Enter Drive Letter manually. >> ..\OfflineUpdater.cmd
+echo :sdisk >> ..\OfflineUpdater.cmd
+echo set /P DrivePath=Enter Drive letter of WINVAYU ^^^(should be X:^^^): >> ..\OfflineUpdater.cmd
+echo if [%%DrivePath%%]==[] goto sdisk >> ..\OfflineUpdater.cmd
+echo if not "%%DrivePath:~1,1%%"==":" set DrivePath=%%DrivePath%%:>> ..\OfflineUpdater.cmd
+echo :start >> ..\OfflineUpdater.cmd
+echo if not exist "%%DrivePath%%\Windows\" echo Error! Selected Disk "%%DrivePath%%" doesn't have any Windows installation. ^& pause ^& exit >> ..\OfflineUpdater.cmd
+echo .\tools\DriverUpdater\%%PROCESSOR_ARCHITECTURE%%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\vayu.xml -p %%DrivePath%% >> ..\OfflineUpdater.cmd
 
 echo apps\IPA\Dialer > filelist_vayu.txt
 echo apps\IPA\Chat >> filelist_vayu.txt
@@ -30,21 +31,14 @@ echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_COMPONENTS >> file
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL >> filelist_vayu.txt
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL_EXTRAS >> filelist_vayu.txt
 echo components\QC8150\Device\DEVICE.SOC_QC8150.VAYU >> filelist_vayu.txt
-echo components\QC8150\Device\DEVICE.SOC_QC8150.VAYU_HUAXING >> filelist_vayu.txt
-echo components\QC8150\Device\DEVICE.SOC_QC8150.VAYU_TIANMA >> filelist_vayu.txt
 echo components\QC8150\Device\DEVICE.SOC_QC8150.VAYU_MINIMAL >> filelist_vayu.txt
 echo components\QC8150\Graphics\GRAPHICS.SOC_QC8150.VAYU_DESKTOP >> filelist_vayu.txt
 echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE >> filelist_vayu.txt
 echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE_MINIMAL >> filelist_vayu.txt
 echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BATTERY >> filelist_vayu.txt
-echo definitions\Desktop\ARM64\Internal\vayu_huaxing.xml >> filelist_vayu.txt
-echo definitions\Desktop\ARM64\Internal\vayu_tianma.xml >> filelist_vayu.txt
-echo definitions\Desktop\ARM64\PE\vayu_huaxing.xml >> filelist_vayu.txt
-echo definitions\Desktop\ARM64\PE\vayu_tianma.xml >> filelist_vayu.txt
+echo definitions\Desktop\ARM64\Internal\vayu.xml >> filelist_vayu.txt
 echo definitions\Desktop\ARM64\PE\vayu.xml >> filelist_vayu.txt
-echo DriverUpdater.ARM64.exe >> filelist_vayu.txt
-echo DriverUpdater.AMD64.exe >> filelist_vayu.txt
-echo DriverUpdater.X86.exe >> filelist_vayu.txt
+echo tools\DriverUpdater >> filelist_vayu.txt
 echo LICENSE.md >> filelist_vayu.txt
 echo OfflineUpdater_huaxing.cmd >> filelist_vayu.txt
 echo OnlineUpdater_huaxing.cmd >> filelist_vayu.txt
@@ -57,11 +51,6 @@ cd ..
 "%ProgramFiles%\7-Zip\7z.exe" a -t7z ..\Vayu-Drivers-Release\Vayu-Drivers-Desktop.7z @tools\filelist_vayu.txt -scsWIN
 cd tools
 
-del ..\OfflineUpdater_huaxing.cmd
-del ..\OnlineUpdater_huaxing.cmd
-del ..\OfflineUpdater_tianma.cmd
-del ..\OnlineUpdater_tianma.cmd
-del ..\DriverUpdater.ARM64.exe
-del ..\DriverUpdater.AMD64.exe 
-del ..\DriverUpdater.X86.exe
+del ..\OfflineUpdater.cmd
+del ..\OnlineUpdater.cmd
 del filelist_vayu.txt
